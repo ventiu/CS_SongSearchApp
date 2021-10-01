981b221307714f5a43c53e4c4ecb9ed814be719a
Tianyou Wen <twen22@wisc.edu>
2021-10-01 00:59:42 +0000

    Added starter files for role Integration Manager

---
+++ SongSearchTests.java
@@ -0,0 +1,16 @@
+public class SongSearchTests {
+
+    public static void main(String[] args) throws Exception {
+        // Run All Tests
+    }
+
+    // Data Wrangler Code Tests
+
+    // Back End Developer Tests
+
+    // Front End Developer Tests
+
+    // Integration Manager Tests
+
+
+}
